c1b660da717af6dc7b3491e6c2a2374d6706ad69
zhubao315 <zhubao315@hotmail.com>
2022-06-16 14:18:02 +0800

    edit config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  pathPrefix: '/', // Change to `/running_page` when running on github pages
+  pathPrefix: '/running', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    siteUrl: 'https://zhubao315.github.io/',
+    logo: 'https://dgalywyr863hv.cloudfront.net/pictures/athletes/339768/24821975/1/large.jpg',
+    description: 'zhubao315',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://zhubao315.github.io/',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://zhubao315.github.io/',
       },
     ],
   },
