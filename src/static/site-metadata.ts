2467b0289575a8dcc934406bc34adf45ec3a59a9
zhubao315 <zhubao315@hotmail.com>
2025-02-04 12:21:09 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,20 +10,16 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  siteTitle: 'zhubao315的跑步生涯',
+  siteUrl: 'https://zhubao315.github.io/running/',
+  logo: 'https://avatars.githubusercontent.com/u/1206487?v=4',
+  description: 'zhubao315的跑步生涯',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: 'X',
+      url: 'https://x.com/zhubao315',
     },
   ],
 };
 
-export default data;
+export default data
